efa7b1375bc8839ac3f6dc41ce99c3abdb8b1866
Leonardo <leonardonatal94@gmail.com>
2022-08-24 14:25:04 -0300

    remove momenttimezone

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
 import { LoggInterceptor } from './common/interceptors/logging.interceptor';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as momentTimeZone from 'moment-timezone';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
@@ -11,11 +10,6 @@ async function bootstrap() {
   app.useGlobalInterceptors(new TimeoutInterceptor());
   app.useGlobalFilters(new AllExceptionsFilter());
 
-  Date.prototype.toJSON = function (): any {
-    return momentTimeZone(this)
-      .tz('America/Sao_Paulo')
-      .format('YYYY-MM-DD HH:mm:ss.SSS');
-  };
   await app.listen(8080);
 }
 bootstrap();
