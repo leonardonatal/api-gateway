aba2286cff032f62bda93dec8ac6e6c8117b1c07
Leonardo <leonardonatal94@gmail.com>
2022-08-24 17:33:55 -0300

    continue lerarning

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ProxyRMQModule } from './proxyrmq/proxyrmq.module';
 import { AwsModule } from './aws/aws.module';
 import { ConfigModule } from '@nestjs/config';
 import { DesafiosModule } from './desafios/desafios.module';
+import { RankingsModule } from './rankings/rankings.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { DesafiosModule } from './desafios/desafios.module';
     AwsModule,
     ConfigModule.forRoot({ isGlobal: true }),
     DesafiosModule,
+    RankingsModule,
   ],
   controllers: [],
   providers: [ClientProxySmartRanking],
---
+++ src/rankings/rankings.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { stringify } from 'querystring';
+
+@Controller('api/v1/rankings')
+export class RankingsController {
+  // @Get()
+  // consultarRankings() {
+  //     @Query('idCategoria') idCategoria: stringify,
+  //     @Query('dataRef') dataRef: string): Observable<any> {}
+  // }
+}
